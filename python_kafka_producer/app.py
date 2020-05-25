4e45f16534c16bb8a700716e29f02b0cffe157c6
Sergio Passos <sergio.passos88@gmail.com>
2020-05-25 01:40:24 -0300

    removendo pasta worker

--- python_kafka_producer/app.py
+++ python_kafka_producer/app.py
@@ -1,25 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2016 Confluent Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-#
-# Example Kafka Producer.
-# Reads lines from stdin and sends to Kafka.
-#
-
 from confluent_kafka import Producer
 import sys
 
@@ -56,7 +34,7 @@ if __name__ == '__main__':
     #       will most likely not serve the delivery callback for the
     #       last produce()d message.
     p.poll(0)
-    
+
     # Wait until all messages have been delivered
     sys.stderr.write('%% Waiting for %d deliveries\n' % len(p))
     p.flush()
